5a9a62b91e0b40bc369cebcc0386f0180e0d97c2
Ryan <eversoryan@gmail.com>
2022-07-12 12:12:17 -0600

    Getting the README anchors to work

--- Develop/index.js
+++ Develop/index.js
@@ -7,30 +7,30 @@ const generateReadMe = function(data) {
     return `# ${data.title}
 
     ## Table of Contents
-    1.[Installation](#installation)
-    2.[Description](#description)
-    3.[Installation](#installation)
-    4.[Usage Information](#usage information)
-    5.[Contribution](#contribution)
-    6.[Test](#test)
-    7.[Questions](#questions)
+    1. [Installation](#installation)
+    2. [Description](#description)
+    3. [Installation](#installation)
+    4. [Usage Information](#usage-information)
+    5. [Contribution](#contribution)
+    6. [Test](#test)
+    7. [Questions](#questions)
     
-    #Description
+    # Description
     ${data.description}
     
-    #Installation
+    # Installation
     ${data.install}
     
-    #Usage Information
+    # Usage Information
     ${data.usage}
     
-    #Contribution
+    # Contribution
     ${data.contribution}
     
-    #Test
+    # Test
     ${data.test}
     
-    #Questions
+    # Questions
     ${data.questions}`
 };
 
